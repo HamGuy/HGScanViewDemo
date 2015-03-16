804e511cf2617f64c2dd7d43356ae3da84492075
wangrui <wangrui@dxyer.com>
2015-03-16 09:43:33 +0800

    Initial Commit

---
+++ HGScanViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HGScanViewDemo
+//
+//  Created by HamGuy on 3/16/15.
+//  Copyright (c) 2015 HamGuy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HGScanViewDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HGScanViewDemo
+//
+//  Created by HamGuy on 3/16/15.
+//  Copyright (c) 2015 HamGuy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
